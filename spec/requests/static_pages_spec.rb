6382cb67c6130e7bf54f52e35c6ad7a06a302574
Venkatesh Tadinada <venkat@solivar.com>
2012-02-11 13:42:18 -0800

    finish Static page

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  describe "Home page" do
+    it "should have the right title" do
+	visit '/static_pages/home'
+	page.should have_selector('title', :text => " | Home")
+    end
+    it "should have the content 'Sample App'" do
+	visit '/static_pages/home'
+	page.should have_selector('h1', :text => "Sample App")
+    end
+
+
+  end
+
+  describe "Help page" do
+    it "should have the right title" do
+	visit '/static_pages/help'
+	page.should have_selector('title', :text => " | Help")
+    end
+    it "should have the content 'Help'" do
+	visit '/static_pages/help'
+	page.should have_selector('h1', :text => "Help")
+    end
+  end
+
+  describe "About page" do
+    it "should have the right title" do
+	visit '/static_pages/about'
+	page.should have_selector('title', :text => " | About")
+    end
+    it "should have the content 'About'" do
+	visit '/static_pages/about'
+	page.should have_selector('h1', :text => "About")
+    end
+  end
+
+end
